7a0648d9292f9ce8560f40943fc9ad786be68a17
+ <nickson.mogaka@student.moringaschool.com>
2024-06-25 08:28:00 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+// Define companyName as a const with the value 'Scuber'
+const companyName = 'Scuber';
+
+// Define mostProfitableNeighborhood using let with the value 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define companyCeo using let with the value 'Susan Smith'
+let companyCeo = 'Susan Smith';
